661b5bff779ffb5a9e470d8817bb8645ec074079
Hasan Zubeir Pohan <hasanpohan035@gmail.com>
2025-06-04 08:43:01 +0700

    save

--- Game.java
+++ Game.java
@@ -1,12 +1,56 @@
-@Override
-public String toString() {
-    return "Game{" +
-            "id=" + id +
-            ", name='" + name + "'" +
-            ", genre='" + genre + "'" +
-            ", rating=" + rating +
-            ", releaseDate='" + releaseDate + "'" +
-            ", popularity=" + popularity +
-            ", description='" + description + "'" +
-            '}';
+public class Game {
+    private int id;
+    private String name;
+    private String genre;
+    private float rating;
+    private String releaseDate;
+    private int popularity;
+    private String description;
+
+    public Game() {}
+
+    public Game(int id, String name, String genre, float rating, String releaseDate, int popularity, String description) {
+        this.id = id;
+        this.name = name;
+        this.genre = genre;
+        this.rating = rating;
+        this.releaseDate = releaseDate;
+        this.popularity = popularity;
+        this.description = description;
+    }
+
+    // Getter dan Setter
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getGenre() { return genre; }
+    public void setGenre(String genre) { this.genre = genre; }
+
+    public float getRating() { return rating; }
+    public void setRating(float rating) { this.rating = rating; }
+
+    public String getReleaseDate() { return releaseDate; }
+    public void setReleaseDate(String releaseDate) { this.releaseDate = releaseDate; }
+
+    public int getPopularity() { return popularity; }
+    public void setPopularity(int popularity) { this.popularity = popularity; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+
+    @Override
+    public String toString() {
+        return "Game{" +
+                "id=" + id +
+                ", name='" + name + "'" +
+                ", genre='" + genre + "'" +
+                ", rating=" + rating +
+                ", releaseDate='" + releaseDate + "'" +
+                ", popularity=" + popularity +
+                ", description='" + description + "'" +
+                '}';
+    }
 }
